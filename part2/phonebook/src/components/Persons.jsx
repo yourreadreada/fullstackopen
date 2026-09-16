6901502531858e2bd27f493f679d3c23369aacad
Your Read Reada <yourreadreada@gmail.com>
2026-09-16 19:19:27 +0530

    Create Persons.jsx

---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,11 @@
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map(person => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ))}
+  </div>
+)
+
+export default Persons
