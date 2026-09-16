ba2f2d92e037a128857e39c9c6e01f73aeca26e6
Your Read Reada <yourreadreada@gmail.com>
2026-09-16 19:16:12 +0530

    Create Course.jsx

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, current) => acc + current.exercises, 0)
+  return <strong>total of {sum} exercises</strong>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
