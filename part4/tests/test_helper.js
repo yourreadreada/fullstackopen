a0bd40c3ad774d2f12e30651c4f1892cdb433fd0
Your Read Reada <yourreadreada@gmail.com>
2026-09-17 09:51:31 +0530

    Update test_helper.js

--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -31,8 +32,14 @@ const blogsInDb = async () => {
   return blogs.map(b => b.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
   initialBlogs,
   nonExistingId,
-  blogsInDb
+  blogsInDb,
+  usersInDb
 }
