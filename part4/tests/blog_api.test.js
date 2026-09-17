ee242620dcb744d2a5b755c11e67717888368aba
Your Read Reada <yourreadreada@gmail.com>
2026-09-17 09:52:58 +0530

    Update blog_api.test.js

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,18 +2,33 @@ const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 describe('when there are initially some blogs saved', () => {
+  let token = null
+  let testUser = null
+
   beforeEach(async () => {
+    await User.deleteMany({})
     await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
+
+    const passwordHash = await bcrypt.hash('secretpassword', 10)
+    testUser = new User({ username: 'testroot', name: 'Tester', passwordHash })
+    await testUser.save()
+
+    const userForToken = { username: testUser.username, id: testUser._id }
+    token = jwt.sign(userForToken, process.env.SECRET || 'secretkey')
+
+    const blogObjects = helper.initialBlogs.map(b => new Blog({ ...b, user: testUser._id }))
+    await Blog.insertMany(blogObjects)
   })
 
-  // Exercise 4.8
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -26,7 +41,6 @@ describe('when there are initially some blogs saved', () => {
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
 
-  // Exercise 4.9
   test('unique identifier property of blog posts is named id', async () => {
     const response = await api.get('/api/blogs')
     const blog = response.body[0]
@@ -35,8 +49,8 @@ describe('when there are initially some blogs saved', () => {
   })
 
   describe('addition of a new blog', () => {
-    // Exercise 4.10
-    test('succeeds with valid data', async () => {
+    // Exercise 4.23: authenticated addition
+    test('succeeds with valid data and token', async () => {
       const newBlog = {
         title: 'Full Stack Open rocks',
         author: 'Matti Luukkainen',
@@ -46,6 +60,7 @@ describe('when there are initially some blogs saved', () => {
 
       await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -57,7 +72,25 @@ describe('when there are initially some blogs saved', () => {
       assert(titles.includes('Full Stack Open rocks'))
     })
 
-    // Exercise 4.11
+    // Exercise 4.23: fails with 401 Unauthorized if token missing
+    test('fails with status code 401 Unauthorized if token is not provided', async () => {
+      const newBlog = {
+        title: 'Unauthorized blog',
+        author: 'Ghost',
+        url: 'https://ghost.org/',
+        likes: 1
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(401)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
+
     test('defaults likes to 0 if likes property is missing', async () => {
       const newBlog = {
         title: 'Blog without likes',
@@ -67,6 +100,7 @@ describe('when there are initially some blogs saved', () => {
 
       const response = await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -74,7 +108,6 @@ describe('when there are initially some blogs saved', () => {
       assert.strictEqual(response.body.likes, 0)
     })
 
-    // Exercise 4.12
     test('fails with status code 400 if title is missing', async () => {
       const newBlog = {
         author: 'No Title',
@@ -83,6 +116,7 @@ describe('when there are initially some blogs saved', () => {
 
       await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(400)
 
@@ -98,6 +132,7 @@ describe('when there are initially some blogs saved', () => {
 
       await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(400)
 
@@ -107,13 +142,13 @@ describe('when there are initially some blogs saved', () => {
   })
 
   describe('deletion of a blog', () => {
-    // Exercise 4.13
-    test('succeeds with status code 204 if id is valid', async () => {
+    test('succeeds with status code 204 if id is valid and user is creator', async () => {
       const blogsAtStart = await helper.blogsInDb()
       const blogToDelete = blogsAtStart[0]
 
       await api
         .delete(`/api/blogs/${blogToDelete.id}`)
+        .set('Authorization', `Bearer ${token}`)
         .expect(204)
 
       const blogsAtEnd = await helper.blogsInDb()
@@ -125,7 +160,6 @@ describe('when there are initially some blogs saved', () => {
   })
 
   describe('updating a blog', () => {
-    // Exercise 4.14
     test('succeeds in updating the number of likes', async () => {
       const blogsAtStart = await helper.blogsInDb()
       const blogToUpdate = blogsAtStart[0]
