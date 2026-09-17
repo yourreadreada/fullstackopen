b780915f57779b04d17dae0608c0dc22f421415a
Your Read Reada <yourreadreada@gmail.com>
2026-09-17 09:15:07 +0530

    Update person.js

--- part3/models/person.js
+++ part3/models/person.js
@@ -15,8 +15,22 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: [3, 'Name must be at least 3 characters long'],
+    required: [true, 'Name is required']
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Phone number must be at least 8 characters long'],
+    required: [true, 'Phone number is required'],
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! Format must be XX-XXXXXXX or XXX-XXXXXXX`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
