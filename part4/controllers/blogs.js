28d2c883436cb84e8ff14d015a21b3fcff602fa1
Your Read Reada <yourreadreada@gmail.com>
2026-09-17 09:50:57 +0530

    Update blogs.js

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,15 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const { userExtractor } = require('../utils/middleware')
 
-// Exercise 4.8: GET all blogs
+// Exercise 4.17: Populate user details
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
-// GET single blog
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+  const blog = await Blog.findById(request.params.id).populate('user', { username: 1, name: 1 })
   if (blog) {
     response.json(blog)
   } else {
@@ -17,9 +20,10 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-// Exercise 4.10, 4.11, 4.12: POST blog
-blogsRouter.post('/', async (request, response) => {
+// Exercise 4.19 & 4.22: POST blog with userExtractor
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
+  const user = request.user
 
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title or url missing' })
@@ -29,20 +33,34 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes === undefined ? 0 : body.likes
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
-// Exercise 4.13: DELETE blog
-blogsRouter.delete('/:id', async (request, response) => {
+// Exercise 4.21 & 4.22: DELETE blog only by creator
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(403).json({ error: 'only the creator can delete this blog' })
+  }
+
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
 
-// Exercise 4.14: PUT update blog
 blogsRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes } = request.body
 
