ad3e13d3cd454a0707fca4a06700d5ee470c617c
Your Read Reada <yourreadreada@gmail.com>
2026-09-17 09:37:51 +0530

    Create test_helper.js

---
+++ part4/tests/test_helper.js
@@ -0,0 +1,38 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: 'Dan Abramov',
+    url: 'http://example.com/html-is-easy',
+    likes: 5
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://example.com/canonical',
+    likes: 12
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: 'willremovethissoon',
+    url: 'http://example.com'
+  })
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(b => b.toJSON())
+}
+
+module.exports = {
+  initialBlogs,
+  nonExistingId,
+  blogsInDb
+}
